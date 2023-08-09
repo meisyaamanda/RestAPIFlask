425f9f553facb72f80195bc279f69e2ea58b0a0e
meisyaamanda <meisyaaldiamanda@gmail.com>
2023-08-09 19:53:07 +0700

    menambahkan resouce

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS = app
+
+# Load model saat aplikasi dimulai
+with open('rf_pickle.pkl', 'rb') as r:
+    model = pickle.load(r, encoding='latin1')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            data = request.json
+
+            accelerations = float(data['accelerations'])
+            movement = float(data['movement'])
+            uterine = float(data['uterine'])
+            light = float(data['light'])
+            severe = float(data['severe'])
+            prolongued = float(data['prolongued'])
+            abnormal = float(data['abnormal'])
+            percentage = float(data['percentage'])
+
+            input_data = np.array((accelerations, movement, uterine, light, severe, prolongued, abnormal, percentage))
+            input_data = np.reshape(input_data, (1, -1))
+
+            isJanin = model.predict(input_data)
+
+            result = {'result': int(isJanin[0])}  # Konversi hasil prediksi ke integer
+
+            return jsonify(result), 200
+
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run()
